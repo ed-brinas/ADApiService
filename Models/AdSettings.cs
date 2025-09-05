b820355d648affa18c4766bf97d0a124205e70e0
ed-brinas <32341952+ed-brinas@users.noreply.github.com>
2025-09-05 11:52:27 +0300

    Update AdSettings.cs

--- Models/AdSettings.cs
+++ Models/AdSettings.cs
@@ -1,24 +1,12 @@
-namespace ADApiService.Models;
-
-public class AdSettings
+namespace ADApiService.Models
 {
-    public string ForestRootDomain { get; set; } = string.Empty;
-    public List<string> Domains { get; set; } = [];
-    public string DefaultUserOuFormat { get; set; } = string.Empty;
-    public string AdminUserOuFormat { get; set; } = string.Empty;
-    public AccessControlSettings AccessControl { get; set; } = new();
-    public ProvisioningSettings Provisioning { get; set; } = new();
+    public class AdSettings
+    {
+        public List<string> Domains { get; set; } = new();
+        public List<string> GeneralAccessGroups { get; set; } = new();
+        public List<string> HighPrivilegeGroups { get; set; } = new();
+        public List<string> OptionalGroupsForHighPrivilege { get; set; } = new();
+        public string AdminGroup { get; set; } = string.Empty;
+    }
 }
 
-public class AccessControlSettings
-{
-    public List<string> GeneralAccessGroups { get; set; } = [];
-    public List<string> HighPrivilegeGroups { get; set; } = [];
-}
-
-public class ProvisioningSettings
-{
-    public List<string> DefaultUserGroups { get; set; } = [];
-    public List<string> OptionalUserGroups { get; set; } = [];
-    public List<string> DefaultAdminGroups { get; set; } = [];
-}
