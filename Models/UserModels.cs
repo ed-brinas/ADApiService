a51c945b52ba9e279496584b2038f29d8c92d2cb
ed-brinas <32341952+ed-brinas@users.noreply.github.com>
2025-09-05 12:27:59 +0300

    Update UserModels.cs

--- Models/UserModels.cs
+++ Models/UserModels.cs
@@ -1,47 +1,48 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace ADApiService.Models;
-
-public class CreateUserModel
+namespace ADApiService.Models
 {
-    [Required]
-    public string Domain { get; set; } = string.Empty;
-    [Required]
-    public string FirstName { get; set; } = string.Empty;
-    [Required]
-    public string LastName { get; set; } = string.Empty;
-    [Required]
-    public string SamAccountName { get; set; } = string.Empty;
-    [Required]
-    public string Password { get; set; } = string.Empty;
-    public List<string> OptionalGroups { get; set; } = new();
-    public bool CreateAdminAccount { get; set; }
+    // This is the definition of CreateUserRequest
+    public class CreateUserRequest
+    {
+        [Required]
+        public string Domain { get; set; } = string.Empty;
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string SamAccountName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        public List<string>? OptionalGroups { get; set; }
+        public bool CreateAdminAccount { get; set; }
+    }
+
+    public class UpdateUserRequest
+    {
+        [Required]
+        public string Domain { get; set; } = string.Empty;
+        [Required]
+        public string SamAccountName { get; set; } = string.Empty;
+        public List<string>? OptionalGroups { get; set; }
+        public bool CreateAdminAccount { get; set; }
+    }
+
+    public class UserDetailModel
+    {
+        public string DisplayName { get; set; } = string.Empty;
+        public string SamAccountName { get; set; } = string.Empty;
+        public bool HasAdminAccount { get; set; }
+        public List<string> MemberOf { get; set; } = new();
+    }
+    
+    public class UserListItem
+    {
+        public string DisplayName { get; set; } = string.Empty;
+        public string SamAccountName { get; set; } = string.Empty;
+        public string? EmailAddress { get; set; }
+        public bool Enabled { get; set; }
+    }
 }
 
-public class UserListItem
-{
-    public string DisplayName { get; set; } = string.Empty;
-    public string SamAccountName { get; set; } = string.Empty;
-    public string? EmailAddress { get; set; }
-    public bool Enabled { get; set; }
-}
-
-// NEW: Full details for a single user for the edit form
-public class UserDetailModel
-{
-    public string DisplayName { get; set; } = string.Empty;
-    public string SamAccountName { get; set; } = string.Empty;
-    public bool HasAdminAccount { get; set; }
-    public List<string> MemberOf { get; set; } = new();
-}
-
-// NEW: Data sent from the frontend to update a user
-public class UpdateUserRequest
-{
-    [Required]
-    public string Domain { get; set; } = string.Empty;
-    [Required]
-    public string SamAccountName { get; set; } = string.Empty;
-    public List<string>? OptionalGroups { get; set; }
-    public bool CreateAdminAccount { get; set; }
-}
