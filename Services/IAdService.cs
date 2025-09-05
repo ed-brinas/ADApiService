0eb3e8c347c79ac4e6167b67427e099e53702df9
ed-brinas <32341952+ed-brinas@users.noreply.github.com>
2025-09-05 12:18:45 +0300

    Update IAdService.cs

--- Services/IAdService.cs
+++ Services/IAdService.cs
@@ -1,14 +1,14 @@
-using System.Security.Principal;
 using ADApiService.Models;
+using System.Security.Claims;
 
-namespace ADApiService.Services;
-
-public interface IAdService
+namespace ADApiService.Services
 {
-    UserContext GetUserContext(IPrincipal user);
-    Task CreateUserAsync(CreateUserRequest request, IPrincipal callingUser);
-    Task<IEnumerable<UserListItem>> ListUsersAsync(string domain, string? nameFilter, bool? statusFilter);
-    Task ResetPasswordAsync(string domain, string samAccountName, string newPassword);
-    Task UnlockAccountAsync(string domain, string samAccountName);
+    public interface IAdService
+    {
+        Task<bool> CreateUserAsync(ClaimsPrincipal callingUser, CreateUserRequest request);
+        Task<bool> UpdateUserAsync(ClaimsPrincipal callingUser, UpdateUserRequest request);
+        Task<UserDetailModel?> GetUserDetailsAsync(string domain, string samAccountName);
+        Task<IEnumerable<UserListItem>> ListUsersAsync(string domain, string? nameFilter, bool? statusFilter);
+    }
 }
 
