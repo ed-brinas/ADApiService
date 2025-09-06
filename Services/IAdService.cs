c983aab9dfda4f009ad5df5f1964c93fa7977c47
ed-brinas <32341952+ed-brinas@users.noreply.github.com>
2025-09-06 12:14:38 +0300

    Update IAdService.cs

--- Services/IAdService.cs
+++ Services/IAdService.cs
@@ -8,7 +8,16 @@ namespace ADApiService.Services;
 /// </summary>
 public interface IAdService
 {
-    Task<IEnumerable<UserListItem>> ListUsersAsync(string domain, string? nameFilter, bool? statusFilter);
+    /// <summary>
+    /// Retrieves a list of users from a specified domain, with optional filters.
+    /// </summary>
+    /// <param name="domain">The domain to search in.</param>
+    /// <param name="nameFilter">A filter for the user's name, username, or email.</param>
+    /// <param name="statusFilter">Filters users by their enabled/disabled status.</param>
+    /// <param name="hasAdminAccountFilter">Filters users based on the existence of an associated admin account.</param>
+    /// <returns>A list of users matching the criteria.</returns>
+    Task<IEnumerable<UserListItem>> ListUsersAsync(string domain, string? nameFilter, bool? statusFilter, bool? hasAdminAccountFilter);
+    
     Task<UserDetailModel?> GetUserDetailsAsync(string domain, string samAccountName);
     Task<CreateUserResponse> CreateUserAsync(ClaimsPrincipal callingUser, CreateUserRequest request);
     Task UpdateUserAsync(ClaimsPrincipal callingUser, UpdateUserRequest request);
@@ -16,4 +25,3 @@ public interface IAdService
     Task UnlockAccountAsync(UserActionRequest request);
     Task DisableAccountAsync(UserActionRequest request);
 }
-
