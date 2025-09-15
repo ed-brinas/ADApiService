f8a5eeba25b95a030e09d2062cd61250b9761260
ed-brinas <32341952+ed-brinas@users.noreply.github.com>
2025-09-15 09:01:25 +0300

    Update delegation.ps1

--- delegation.ps1
+++ delegation.ps1
@@ -1,102 +1,64 @@
 # --- Configuration ---
-# The distinguished name of the OU in the CHILD domain.
-$targetOU = "OU=_Managed,DC=new,DC=lab,DC=local" 
+# The distinguished name of the OU in the CHILD domain where permissions will be applied.
+$targetOU = "OU=_AdminAccounts,DC=new,DC=lab,DC=local" 
 
-# The SamAccountName of the service account in the PARENT domain.
+# The SamAccountName of the service account which exists in the PARENT domain.
 $serviceAccount = "svc_adapi"
 
 # The fully qualified domain name of a domain controller in the PARENT domain.
-$parentDomainController = "DC01.lab.local" 
-
-# The fully qualified domain name of a domain controller in the CHILD domain.
-$childDomainController = "NEWDC01.new.lab.local"
+$parentDomainController = "DC-03.lab.local"
 
 # --- Main Script ---
 
-# Step 1: Get the service account's SID from the PARENT domain.
+# Step 1: Get the service account's SID from the PARENT domain controller.
 Write-Host "Fetching SID for '$serviceAccount' from '$parentDomainController'..."
 $sid = (Get-ADUser -Identity $serviceAccount -Server $parentDomainController).SID
 if ($null -eq $sid) {
-    Write-Error "Could not find service account '$serviceAccount' in domain '$parentDomainController'. Please check the name and domain."
+    Write-Error "Could not find service account '$serviceAccount' in domain '$parentDomainController'. Please check the name and domain controller."
     return
 }
+Write-Host "Successfully found SID for '$serviceAccount'."
 
-# Step 2: Get the current ACL for the target OU from the CHILD domain.
-Write-Host "Fetching ACL for '$targetOU' from '$childDomainController'..."
-$acl = Get-Acl "AD:\$targetOU" -Server $childDomainController
+# Step 2: Get the current ACL from the target OU. 
+# The AD provider automatically routes this to the correct domain controller based on the DN.
+Write-Host "Fetching ACL for '$targetOU'..."
+$acl = Get-Acl "AD:\$targetOU"
 
-# ... (The rest of the script for defining and adding the access rules remains exactly the same) ...
+# Step 3: Define all the necessary permissions (Access Control Entries).
 
-# 1. Permission to Create User Objects in this OU
-$objectType = [System.Guid]::new("bf967aba-0de6-11d0-a329-00c04fd8d5cd") # GUID for "User" object type
-$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-    $sid, 
-    "CreateChild", 
-    "Allow", 
-    $objectType, 
-    "Descendents"
-)
+# 3.1: Permission to Create User Objects in this OU
+$objectType = [System.Guid]::new("bf967aba-0de6-11d0-a329-00c04fd8d5cd") # GUID for "User" object
+$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($sid, "CreateChild", "Allow", $objectType, "Descendents")
 $acl.AddAccessRule($ace)
-Write-Host "Granted: Create User Objects"
+Write-Host "Rule Added: Create User Objects"
 
-# 2. Permission to Reset User Passwords
-$objectType = [System.Guid]::new("00299570-246d-11d0-a768-00aa006e0529") # GUID for "User" object type
+# 3.2: Permission to Reset User Passwords
 $rights = [System.DirectoryServices.ActiveDirectoryRights]::ExtendedRight
-$extendedRightGuid = [System.Guid]::new("00299571-280d-11d1-a768-00aa006e0529") # GUID for "Reset Password" extended right
-$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-    $sid, 
-    $rights, 
-    "Allow", 
-    $extendedRightGuid, 
-    "Descendents"
-)
+$extendedRightGuid = [System.Guid]::new("00299571-280d-11d1-a768-00aa006e0529") # GUID for "Reset Password"
+$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($sid, $rights, "Allow", $extendedRightGuid, "Descendents")
 $acl.AddAccessRule($ace)
-Write-Host "Granted: Reset User Passwords"
+Write-Host "Rule Added: Reset User Passwords"
 
-# 3. Permission to Modify User Account Properties
-$objectType = [System.Guid]::new("bf967aba-0de6-11d0-a329-00c04fd8d5cd") # GUID for "User" object type
+# 3.3: Permission to Modify User Account Properties (Enable/Disable, Expiration)
 $rights = "WriteProperty"
-$propertyGuid = [System.Guid]::new("4c164200-20c0-11d0-a768-00aa006e0529") # GUID for "Account Restrictions" property set
-$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-    $sid, 
-    $rights, 
-    "Allow", 
-    $propertyGuid, 
-    "Descendents"
-)
+$propertyGuid = [System.Guid]::new("4c164200-20c0-11d0-a768-00aa006e0529") # GUID for "Account Restrictions"
+$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($sid, $rights, "Allow", $propertyGuid, "Descendents")
 $acl.AddAccessRule($ace)
-Write-Host "Granted: Modify User Account Properties"
+Write-Host "Rule Added: Modify User Account Properties"
 
-# 4. Permission to Modify User's Public Information
-$objectType = [System.Guid]::new("bf967aba-0de6-11d0-a329-00c04fd8d5cd") # GUID for "User" object type
-$rights = "WriteProperty"
-$propertyGuid = [System.Guid]::new("e45795b2-9455-11d1-aebd-0000f80367c1") # GUID for "Public Information" property set
-$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-    $sid, 
-    $rights, 
-    "Allow", 
-    $propertyGuid, 
-    "Descendents"
-)
+# 3.4: Permission to Modify User's Public Information (Display Name, etc.)
+$propertyGuid = [System.Guid]::new("e45795b2-9455-11d1-aebd-0000f80367c1") # GUID for "Public Information"
+$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($sid, $rights, "Allow", $propertyGuid, "Descendents")
 $acl.AddAccessRule($ace)
-Write-Host "Granted: Modify User Public Information"
+Write-Host "Rule Added: Modify User Public Information"
 
-# 5. Permission to Add/Remove Users from Groups
-$objectType = [System.Guid]::new("bf967a9c-0de6-11d0-a329-00c04fd8d5cd") # GUID for "Group" object type
-$rights = "WriteProperty"
-$propertyGuid = [System.Guid]::new("bf9679c0-0de6-11d0-a329-00c04fd8d5cd") # GUID for "Member" attribute
-$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-    $sid, 
-    $rights, 
-    "Allow", 
-    $propertyGuid, 
-    "Descendents"
-)
+# 3.5: Permission to Add/Remove Users from Groups
+$propertyGuid = [System.Guid]::new("bf9679c0-0de6-11d0-a329-00c04fd8d5cd") # GUID for "Member" attribute of a group
+$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($sid, $rights, "Allow", $propertyGuid, "Descendents")
 $acl.AddAccessRule($ace)
-Write-Host "Granted: Modify Group Membership"
+Write-Host "Rule Added: Modify Group Membership"
 
-
-# Step 3: Apply the new ACL to the OU in the CHILD domain.
-Write-Host "Applying new ACL to '$targetOU' on '$childDomainController'..."
-Set-Acl -Path "AD:\$targetOU" -AclObject $acl -Server $childDomainController
+# Step 4: Apply the modified ACL to the OU.
+Write-Host "Applying new ACL to '$targetOU'..."
+Set-Acl -Path "AD:\$targetOU" -AclObject $acl
 Write-Host "Delegation complete for '$serviceAccount' on '$targetOU'."
